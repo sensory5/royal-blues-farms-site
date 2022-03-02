4a7070f6240c7e07f2a2384a63a174cadcf3b5db
Amanda Pocchia <amanda@sensory5.com>
2022-03-02 11:11:35 -0500

    connecting modal, adding content

--- components/recipe.jsx
+++ components/recipe.jsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
 import Image from "next/image";
 
-export default function Recipe({src, alt, children}) {
+export default function Recipe({src, alt, href, children}) {
    return (
        <div class="col-md-6 rbf-recipe">
          <div className="rbf-recipe-img">
-             <Image
-               src={`/images/${src}`}
-               alt={alt}
-               width={660}
-               height={470}
-             />
-             <div className="text-center">
+            <a data-toggle="modal" href={href}>
+               <Image
+                 src={`/images/${src}`}
+                 alt={alt}
+                 width={660}
+                 height={470}
+               />
+            </a>
+             <div className="rbf-recipe-modal text-center">
                 {children}
             </div>
          </div>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
 import Button from "../components/button";
 import Hero from "../components/hero";
@@ -81,17 +82,112 @@ export default function Home() {
           height={78}
         />
 
-        <Recipes title="Our Favorite Blueberry Recipes" text="Click to view recipes">
+        <Recipes title="Our Favorite Blueberry Recipes" text="Click to view recipes" href="#RecipeModal1">
             <Recipe src="img-crumble-recipe-1.jpg" alt="Blueberry Crumble Recipe">
-              <button type="button" class="btn btn-link text-uppercase" data-bs-toggle="modal" data-bs-target="#recipeModal1">
+              <button type="button" className="btn btn-link text-uppercase" data-bs-toggle="modal" data-bs-target="#RecipeModal1">
                 Blueberry Crumble Bars Recipe
               </button>
+              <Modal id="RecipeModal1">
+                <Image
+                  src="/images/img-crumble-recipe-2.jpg"
+                  alt="Blueberry Crumble Bar Recipe"
+                  width={1183}
+                  height={439}
+                />
+
+                <div className="rbf-modal-content text-start p-4">
+                  <h3 className="text-uppercase fw-bold">Blueberry Crumble Bars Recipe</h3>
+
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <p className="text-uppercase mb-1">For the Crust:</p>
+                      <ul className="list-unstyled">
+                        <li>1 cup all-purpose flour</li>
+                        <li>2 cups graham cracker crumbs</li>
+                        <li>½ cup sugar</li>
+                        <li>½ cup brown sugar</li>
+                        <li>1 tsp baking powder</li>
+                        <li>¼ tsp salt</li>
+                        <li>1 egg</li>
+                        <li>½ tsp vanilla</li>
+                        <li>1 cup butter, chilled cut in cubes</li>
+                      </ul>
+                    </div>
+                    <div className="col-lg-6">
+                      <p className="text-uppercase mb-1">Filling:</p>
+                      <ul className="list-unstyled">
+                        <li>1 tbsp cornstarch</li>
+                        <li>½ cup sugar</li>
+                        <li>1 tbsp lemon juice</li>
+                        <li>4 cups fresh blueberries</li>
+                      </ul>
+                    </div>
+                    <div className="col-12 mt-3">
+                      <p className="text-uppercase mb-1">Directions:</p>
+                      <ol>
+                        <li>Preheat the oven to 350F. Line a 9x13 baking dish with parchment paper.</li>
+                        <li>In a mixer, mix all crust ingredients until mixture starts to clump into balls.</li>
+                        <li>Press 3/4 of the mixture into the baking dish and press to create an even layer of the crust.</li>
+                        <li>Mix all of the filling ingredients in a mixing bowl and stir until the berries are evenly coated.</li>
+                        <li>Spread the filling over the crust evenly.</li>
+                        <li>Sprinkle the remainder of the crust on top to create a crumble topper.</li>
+                        <li>Bake for 35-40 minutes, when the crumble appears golden.</li>
+                        <li>Let this sit until the crumble has cooled and set before cutting into bars (you can even place in the fridge to help it set more before cutting into squares or bars).</li>
+                        <li>Enjoy for breakfast or as a dessert with vanilla ice cream.</li>
+                      </ol>
+                    </div>
+                  </div>
+                </div>
+              </Modal>
             </Recipe>
 
-            <Recipe src="img-creamsicle-recipe-1.jpg" alt="Blueberry Lemon Creamsicle Recipe">
-              <button type="button" class="btn btn-link text-uppercase" data-bs-toggle="modal" data-bs-target="#recipeModal2">
+            <Recipe src="img-creamsicle-recipe-1.jpg" alt="Blueberry Lemon Creamsicle Recipe" href="#RecipeModal2">
+              <button type="button" className="btn btn-link text-uppercase" data-bs-toggle="modal" data-bs-target="#RecipeModal2">
                 Blueberry Lemon Creamsicle Recipe
               </button>
+              <Modal id="RecipeModal2">
+                <Image
+                  src="/images/img-creamsicle-recipe-2.jpg"
+                  alt="Blueberry Lemon Creamsicle Recipe"
+                  width={1183}
+                  height={439}
+                />
+
+                <div className="rbf-modal-content text-start p-4">
+                  <h3 className="text-uppercase fw-bold">Blueberry Lemon Creamsicle Recipe</h3>
+
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <p className="text-uppercase mb-1">Blueberry Mixture:</p>
+                      <ul className="list-unstyled">
+                        <li>2 cups blueberries (fresh or frozen)</li>
+                        <li>2 tbsp lemon juice</li>
+                        <li>2 tbsp water</li>
+                        <li>¼ cup sugar</li>
+                      </ul>
+                    </div>
+                    <div className="col-lg-6">
+                      <p className="text-uppercase mb-1">Cream Layer:</p>
+                      <ul className="list-unstyled">
+                        <li>1 14oz can sweetened condensed milk</li>
+                        <li>2 cups milk (can substitute coconut milk)</li>
+                        <li>Juice from 2 lemons</li>
+                      </ul>
+                    </div>
+                    <div className="col-12 mt-3">
+                      <p className="text-uppercase mb-1">Directions:</p>
+                      <ol>
+                        <li>Combine all blueberry mixture ingredients in a saucepan. Cook on medium heat until berries cook down and can be smashed with a fork (about 29 minutes). Let cool.</li>
+                        <li>In a mixing bowl, combine the milk and condensed milk and stir until completely mixed. Stir in lemon juice. You can add more lemon if you like it tart.</li>
+                        <li>Pour cream mixture about 2/3 full in each popsicle mold.</li>
+                        <li>Put 1-2 spoonfuls of blueberry mixture in each mold. Store with a popsicle stick to swirl the mixtures together.</li>
+                        <li>Freeze and enjoy.</li>
+                      </ol>
+                      <p class="small">Yield: 10-12 popsicles</p>
+                    </div>
+                  </div>
+                </div>
+              </Modal>
             </Recipe>
         </Recipes>
 
