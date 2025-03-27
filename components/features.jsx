ced6aa96b277551d5f36a6bcd36a8442b55d9a07
Patrick Ward <patrick@octetic.com>
2025-03-27 10:59:56 -0400

    update upick dates, add social media message

--- components/features.jsx
+++ components/features.jsx
@@ -1,6 +1,6 @@
 export default function Features({children}) {
     return (
-        <div className="container my-5 mb-0 mb-lg-5" id="featured-1">
+        <div className="container my-5 mb-0 mb-xl-5" id="featured-1">
             <div className="row justify-content-center">
                 {children}
             </div>
