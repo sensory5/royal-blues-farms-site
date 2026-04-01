a16d1a5e14af26a8fe5f8077a5551028980769fd
patrickward <patrickward@users.noreply.github.com>
2026-04-01 08:32:55 -0400

    fix: upgrading nextjs too 14 and fixing discrepancies

--- components/hero.jsx
+++ components/hero.jsx
@@ -4,7 +4,7 @@ export default function Hero({srcLogo, widthLogo, heightLogo, altLogo, src, alt}
     return (
         <div className="rbf-hero">
             <div className="rbf-hero-img">
-                <Image src={`/images/${src}`} layout="fill" objectFit="cover" alt={alt} />
+                <Image src={`/images/${src}`} fill style={{objectFit:"cover"}} alt={alt} />
             </div>
             <div className="rbf-hero-overlay p-4 text-center">
                 <Image src={`/images/${srcLogo}`} width={widthLogo} height={heightLogo} alt={altLogo} />
