335b758d3d70d3f5f23e3090041063447d05e0a0
Amanda Pocchia <amanda@sensory5.com>
2026-04-01 11:31:08 -0400

    fixed recipe img styles, logo height

--- components/recipe.jsx
+++ components/recipe.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Recipe({src, alt, target, children}) {
    return (
        <div className="col-lg-6 rbf-recipe mb-4 mb-lg-0">
-         <div className="rbf-recipe-img">
+         <div className="rbf-recipe-img" style={{"position": "relative"}} key={alt}>
             <a data-bs-toggle="modal" data-bs-target={target} href="#">
                <Image
                  src={`/images/${src}`}
@@ -13,7 +13,7 @@ export default function Recipe({src, alt, target, children}) {
                  height={470}
                />
             </a>
-             <div className="rbf-recipe-modal text-center">
+             <div className="rbf-recipe-modal text-center mt-2">
                 {children}
             </div>
          </div>
